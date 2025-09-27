0a1558bfdc1904190d7c4014ead41a6581d70afa
Barbara <baropadi@gmail.com>
2025-09-27 19:05:02 -0300

    chore: create initial file structure for components and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+// import About from './pages/About.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  // { path: '/about', element: <About /> },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Bienvenido a Nimbus Technology</h1>
+      <p>Ofrecemos soluciones tecnológicas para empresas y particulares.</p>
+    </div>
+  )
+}
+
+export default Home
